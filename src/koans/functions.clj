872f8244719eee3218ae9fa98d5a3dc8c67b264a
Patrick Ewing <patrick@twitter.com>
2011-12-03 13:44:05 -0800

    Finished conditionals, functions, high-order functions and maps.

--- src/koans/functions.clj
+++ src/koans/functions.clj
@@ -5,25 +5,26 @@
 
 (meditations
   "Functions are often defined before they are used"
-  (= __ (multiply-by-ten 2))
+  (= 20 (multiply-by-ten 2))
 
   "But they can also be defined inline"
-  (= __ ((fn [n] (* __ n)) 2))
+  (= 4 ((fn [n] (* n n)) 2))
 
   "Or using even shorter syntax"
-  (= __ (#(* 15 %) __))
+  (= 30 (#(* 15 %) 2))
 
   "Short anonymous functions may take multiple arguments"
-  (= __ (#(+ %1 %2 %3) 4 5 6))
+  (= 15 (#(+ %1 %2 %3) 4 5 6))
 
   "One function can beget another"
-  (= __ ((fn []
-           ((fn [a b] (__ a b))
+  (= 9 ((fn []
+           ((fn [a b] (+ a b))
             4 5))))
 
   "Higher-order functions take function arguments"
-  (= 25 (___
-         (fn [n] (* n n))))
+  (= 25 ((fn [f] (f 5))  ; anonymous function that takes a function and calls it with 5 as its argument
+         (fn [n] (* n n))   ; calling said function with another anonymous function that squares its argument
+      ))
 
   "But they are often better written using the names of functions"
-  (= 25 (___ square)))
+  (= 25 ((fn [f] (f 5)) square)))
